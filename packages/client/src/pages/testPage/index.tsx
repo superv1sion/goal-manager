021a094a691d97fdc3d7197beddf67f537dd64d0
grigor.oganesyan <grigor@primary-portal.com>
2024-04-01 14:32:17 +0200

    feat: add plan module, add base repository, add basic CR for plan

--- packages/client/src/pages/testPage/index.tsx
+++ packages/client/src/pages/testPage/index.tsx
@@ -3,14 +3,35 @@ import { ReactElement, useEffect, useState } from 'react'
 import { API_URL } from '@/constants/api'
 
 const TestPage = (): ReactElement => {
-  const [serverHello, setServerHello] = useState('')
-  useEffect(() => {
-    void fetch(API_URL, { method: 'GET' }).then(async (data) => {
+  const [plans, setPlans] = useState('')
+  const getAllPlans = () => {
+    void fetch(API_URL + 'plans', { method: 'GET' }).then(async (data) => {
       const response = await data.json()
-      setServerHello(response.message)
+      setPlans(JSON.stringify(response))
     })
+  }
+
+  useEffect(() => {
+    getAllPlans()
   }, [])
-  return <div>testPage: {serverHello}</div>
+  const addPlan = async (): Promise<any> => {
+    void fetch(API_URL + 'plans', {
+      method: 'POST',
+      body: JSON.stringify({ name: 'testName', userId: 'testUserId' }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(async (data) => {
+      getAllPlans()
+    })
+  }
+
+  return (
+    <div>
+      plans: {plans}
+      <button onClick={addPlan}> add plan </button>
+    </div>
+  )
 }
 
 export default TestPage
