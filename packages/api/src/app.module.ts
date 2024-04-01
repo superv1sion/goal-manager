7f24f76b605f8329d4b91a2ce8dd86350c51c268
grigor.oganesyan <grigor@primary-portal.com>
2024-04-01 14:33:05 +0200

    chore: modules and etc

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlanModule } from './plan/plan.module';
 
 @Module({
-  imports: [],
+  imports: [PlanModule],
   controllers: [AppController],
   providers: [AppService],
 })
