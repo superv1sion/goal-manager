a8907a9cbf6702ef6e601590a0f53fb899b4e443
grigor.oganesyan <grigor@primary-portal.com>
2024-06-27 12:28:22 +0200

    refactoring

--- packages/client/src/app/layout.tsx
+++ packages/client/src/app/layout.tsx
@@ -4,8 +4,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import React from 'react'
 
-import { store, StoreContext } from '@/store/stepsStore'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -20,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
